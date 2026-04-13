7181970a1620cac8733a62b20154d97260d51b00
spiyush0610 <piyush.prabhakar@stage.in>
2026-04-13 10:56:39 +0000

    Deploy patch from ca1adfe189d13a22053dd62151539e1e5fc288f5

--- patch.js
+++ patch.js
@@ -1,7 +1,7 @@
 bridge.override(
     "in.stage.presentation.preview.SharedContentPreviewViewModel",
     "getTitleColor()J",
-    function(args) { return -65536; } // 0xFFFF0000 = Red
+    function(args) { return -16776961; } // 0xFF0000FF = Blue
 );
 
 bridge.override(
