8ba6a509941dc2031f152ec7a7e2915f9018092f
spiyush0610 <piyush.prabhakar@stage.in>
2026-04-13 11:25:12 +0000

    Deploy patch from 310126d283ba19415474b9219d558553e4a862c4

--- patch.js
+++ patch.js
@@ -1,11 +1,1 @@
-bridge.override(
-    "in.stage.presentation.preview.SharedContentPreviewViewModel",
-    "getTitleColor()J",
-    function(args) { return -16776961; } // 0xFF0000FF = Blue
-);
-
-bridge.override(
-    "in.stage.presentation.preview.SharedContentPreviewViewModel",
-    "isDescriptionVisible()Z",
-    function(args) { return false; }
-);
+// no patch
