64a647d502c38bb79a0fa7b2828ff8c446d70c74
spiyush0610 <piyush.prabhakar@stage.in>
2026-04-13 09:39:34 +0000

    Deploy patch from 4baf032e7fcbf0125902a11e9a20ed0c497de7b8

--- patch.js
+++ patch.js
@@ -1,1 +1,8 @@
-// no patch
+bridge.override(
+    "in.stage.presentation.dynamicHome.viewModels.DynamicHomeViewModel",
+    "getHomeScreenLabel()Ljava/lang/String;",
+    function(args) {
+        bridge.log("OTA PATCH APPLIED — getHomeScreenLabel intercepted!");
+        return "PATCHED LABEL!";
+    }
+);
