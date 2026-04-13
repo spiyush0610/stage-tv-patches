f0533ccef8156dc1f38778799b48ce3a181aff80
spiyush0610 <piyush.prabhakar@stage.in>
2026-04-13 12:14:40 +0000

    Deploy patch from 363104d2723528772a6acf9d0cf9ed0238ea6458

--- patch.js
+++ patch.js
@@ -1,5 +1,8 @@
 bridge.override(
-    "in.stage.presentation.settings.viewModels.SettingsViewModel",
-    "getLanguageCodes()Ljava/lang/String;",
-    function(args) { return "en,hi,mr"; }
+    "in.stage.presentation.preview.SharedContentPreviewViewModel",
+    "getHomeRowPriority(Ljava/lang/String;)I",
+    function(args) {
+        if (args[0] === "continueWatching") return 0;
+        return 2147483647;
+    }
 );
