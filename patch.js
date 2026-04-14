6b6f166d3b2f984b9ed5969790d56563304952c4
spiyush0610 <piyush.prabhakar@stage.in>
2026-04-14 04:42:15 +0000

    Deploy patch from f63f1f5c6349c246dbf1258fd384982cc44c3b54

--- patch.js
+++ patch.js
@@ -1,5 +1,1 @@
-bridge.override(                                                                                                                                                                   
-      "in.stage.presentation.settings.viewModels.SettingsViewModel",                                                                                                                 
-      "logout()V",                                                                                                                                                                   
-      function(args) { return null; }                                                                                                                                              
-  );
+
